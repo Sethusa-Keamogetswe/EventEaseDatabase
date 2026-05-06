3518f3bd7be19cd21a31f4d415c229d345d93003
ST10452102 <164493462+Sethusa-Keamogetswe@users.noreply.github.com>
2026-05-06 15:31:39 +0200

    first commit

---
+++ EventEase/Models/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using EventEase.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace EventEase.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        internal readonly object BookingDetailsView;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Venue> Venue { get; set; }
+        public DbSet<Event> Event { get; set; }
+        public DbSet<Booking> Booking { get; set; }
+
+        public DbSet<BookingDetailsView> BookingDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<BookingDetailsView>()
+                .ToView("vw_BookingDetails")
+                .HasNoKey();
+        }
+
+    }
+}
+
